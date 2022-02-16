4f74196dfa44afc06819db1700eeddb18ce7a08e
Yehor <eli0t1326@gmail.com>
2022-02-16 21:47:46 +0200

    Refactor ScriptExecutionContext code

--- src/ScriptExecutionContext.cpp
+++ src/ScriptExecutionContext.cpp
@@ -1,44 +1,50 @@
 #include "ScriptExecutionContext.hpp"
 
+namespace
+{
+    v8::Isolate* createIsolate(v8::ArrayBuffer::Allocator* allocator)
+    {
+        v8::Isolate::CreateParams create_params;
+        create_params.array_buffer_allocator = allocator;
+
+        return v8::Isolate::New(create_params);
+    }
+}
+
 namespace cardan
 {
-    ScriptExecutionContext::ScriptExecutionContext(const std::string& src)
-        : m_jsCode(src)
+    void ScriptExecutionContext::IsolateDisposer::operator() (v8::Isolate* isolate)
     {
-        m_arrayBuffer = std::unique_ptr<v8::ArrayBuffer::Allocator>(v8::ArrayBuffer::Allocator::NewDefaultAllocator());
-
-        v8::Isolate::CreateParams create_params;
-        create_params.array_buffer_allocator = m_arrayBuffer.get();
-        m_isolate = v8::Isolate::New(create_params);
+        isolate->Dispose();
     }
 
-    ScriptExecutionContext::~ScriptExecutionContext()
+    ScriptExecutionContext::ScriptExecutionContext(const std::string& src)
+        : m_jsCode(src)
+        , m_arrayBufferAllocator(v8::ArrayBuffer::Allocator::NewDefaultAllocator())
+        , m_isolate(createIsolate(m_arrayBufferAllocator.get()))
+        , m_isolateScope(m_isolate.get())
+        , m_handleScope(m_isolate.get())
+        , m_context(v8::Context::New(m_isolate.get()))
+        , m_contextScope(m_context)
     {
-        m_isolate->Dispose();
     }
 
     std::variant<std::string, int> ScriptExecutionContext::runScript()
     {
-        v8::Isolate::Scope isolate_scope(m_isolate);
-        v8::HandleScope    handle_scope(m_isolate);
-
-        v8::Local<v8::Context> context = v8::Context::New(m_isolate);
-        v8::Context::Scope context_scope(context);
-
         v8::Local<v8::String> source =
-            v8::String::NewFromUtf8(m_isolate, m_jsCode.c_str(), v8::NewStringType::kNormal).ToLocalChecked();
+            v8::String::NewFromUtf8(m_isolate.get(), m_jsCode.c_str(), v8::NewStringType::kNormal).ToLocalChecked();
 
-        v8::Local<v8::Script> script = v8::Script::Compile(context, source).ToLocalChecked();
+        v8::Local<v8::Script> script = v8::Script::Compile(m_context, source).ToLocalChecked();
 
-        v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-        // Dispose the isolate and tear down V8.
+        v8::Local<v8::Value> result = script->Run(m_context).ToLocalChecked();
+
         if (result->IsInt32())
         {
-            return result->ToInt32(context).ToLocalChecked()->Value();
+            return result->ToInt32(m_context).ToLocalChecked()->Value();
         }
         else if (result->IsString())
         {
-            v8::String::Utf8Value utf8String(m_isolate, result);
+            v8::String::Utf8Value utf8String(m_isolate.get(), result);
             return *utf8String;
         }
     }
--- src/ScriptExecutionContext.hpp
+++ src/ScriptExecutionContext.hpp
@@ -12,12 +12,24 @@ namespace cardan
     {
     public:
         ScriptExecutionContext(const std::string& src);
-        ~ScriptExecutionContext();
 
         std::variant<std::string, int> runScript();
+
     private:
         const std::string m_jsCode;
-        std::unique_ptr<v8::ArrayBuffer::Allocator> m_arrayBuffer;
-        v8::Isolate* m_isolate;
+        std::unique_ptr<v8::ArrayBuffer::Allocator> m_arrayBufferAllocator;
+
+        struct IsolateDisposer
+        {
+            void operator() (v8::Isolate* isolate);
+        };
+
+        std::unique_ptr<v8::Isolate, IsolateDisposer> m_isolate;
+
+        v8::Isolate::Scope m_isolateScope;
+        v8::HandleScope    m_handleScope;
+
+        v8::Local<v8::Context> m_context;
+        v8::Context::Scope m_contextScope;
     };
 }
