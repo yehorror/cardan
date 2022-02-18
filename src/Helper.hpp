7a15d22d1ce60ef99e026b14073aab2d7a13f139
Yehor <eli0t1326@gmail.com>
2022-02-18 22:43:20 +0200

    Refactor helper functions a bit & add some comments and TODO's

--- src/Helper.hpp
+++ src/Helper.hpp
@@ -5,6 +5,8 @@
 namespace cardan::details
 {
     // TODO Consider moving convert functions into separate header
+
+    // Function overloads to convert JS call arguments into C++ functions arguments
     static void convertArgumentFromV8Value(v8::Local<v8::Context> context, v8::Local<v8::Value> value, int& out)
     {
         out = value->Int32Value(context).ToChecked();
@@ -15,6 +17,22 @@ namespace cardan::details
         out = *v8::String::Utf8Value(context->GetIsolate(), value);
     }
 
+    // Function overloads to convert C++ return values into JS return values
+
+    template <class T>
+    static void convertValueToV8ReturnValue(v8::Isolate* isolate, T value, v8::ReturnValue<v8::Value>& returnValue)
+    {
+        // We can rely on SFINAE here
+        returnValue.Set(value);
+    }
+
+    static void convertValueToV8ReturnValue(v8::Isolate* isolate, const std::string& value, v8::ReturnValue<v8::Value>& returnValue)
+    {
+        returnValue.Set(v8::String::NewFromUtf8(isolate, value.c_str()).ToLocalChecked());
+    }
+
+    //------------------------------------------------------------------------------------
+
     template <size_t Idx=0, class... Args>
     static void packArgumentsHelper(std::tuple<Args...>& tuple, const v8::FunctionCallbackInfo<v8::Value>& info)
     {
@@ -23,11 +41,6 @@ namespace cardan::details
         {
             packArgumentsHelper<Idx + 1, Args...>(tuple, info);
         }
-    }
-
-    template <>
-    void packArgumentsHelper(std::tuple<>& tuple, const v8::FunctionCallbackInfo<v8::Value>& info)
-    {
     }
 
     template <class... Args>
@@ -42,19 +55,5 @@ namespace cardan::details
     std::tuple<> packArguments(const v8::FunctionCallbackInfo<v8::Value>& info)
     {
         return std::tuple<>();
-    }
-
-    // TODO Consider moving convert functions into separate header
-    template <class T>
-    static void convertValueToV8ReturnValue(v8::Isolate* isolate, T value, v8::ReturnValue<v8::Value>& returnValue)
-    {
-        // We can rely on SFINAE here
-        // It won't be a compile error if substitution of value type fails
-        returnValue.Set(value);
-    }
-
-    static void convertValueToV8ReturnValue(v8::Isolate* isolate, const std::string& value, v8::ReturnValue<v8::Value>& returnValue)
-    {
-        returnValue.Set(v8::String::NewFromUtf8(isolate, value.c_str()).ToLocalChecked());
     }
 }
