95a8e3be8a407432daff12feed74ae3a7eecc1d1
Yehor <eli0t1326@gmail.com>
2022-04-03 20:17:29 +0300

    Move scenarios with user defined converters to separate folder

---
+++ test/Scenarios/UserDefinedConverters.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Converters/FromV8.hpp"
+#include "Converters/ToV8.hpp"
+
+struct Person
+{
+    std::string name;
+    int age;
+};
+
+namespace cardan::ToV8
+{
+    v8::Local<v8::Value> convert(v8::Local<v8::Context> context, const Person& person);
+}
+
+namespace cardan::FromV8
+{
+    template <>
+    Person convert(v8::Local<v8::Context> context, v8::Local<v8::Value> value);
+}
